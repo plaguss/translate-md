b39e942a6639b8dfaa8639aba33d78c53162f56b
agustin <agustin.piqueres@gmail.com>
2023-03-02 20:32:07 +0100

    feat: requests based client first version

--- translate_md/client.py
+++ translate_md/client.py
@@ -5,23 +5,26 @@ ref:
 https://stackoverflow.com/questions/42009202/how-to-call-a-async-function-contained-in-a-class
 """
 
-import asyncio
-import aiohttp
+from urllib.parse import urljoin
 import json
-
+from typing import Any
 import translate_md.markdown as md
+
+import requests
 
 SPANGLISH_URL = r"http://localhost:8000/"
 
 
 class SpanglishClient:
     def __init__(self) -> None:
-        self._timeout = 300
         self._spanglish_url = SPANGLISH_URL
-        self._loop = asyncio.get_event_loop()
+
+    def translate(self, text: str) -> str:
+        return self._request("/single", payload=text)
 
     def translate_batch(self, texts: list[str]) -> str:
-        return self._loop.run_until_complete(make_request(self._spanglish_url, texts))
+        # json encode the request parameters
+        return self._request("/batch", payload=json.dumps(texts))
 
     def translate_file(self, filename: str) -> None:
         raise NotImplementedError
@@ -29,11 +32,23 @@ class SpanglishClient:
     def __repr__(self) -> str:
         return type(self).__name__ + f"({self._spanglish_url})"
 
+    def _request(self, endpoint: str, payload: str) -> Any:
+        """_summary_
 
-async def make_request(url: str, params: list[str]):
-    payload = json.dumps(params)
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url, params={"text": payload}) as response:
-            response = await response.json()
-            return response
+        Args:
+            endpoint (str): TODO. Endpoint of the app.
+            payload (str): TODO. Document according to the app.
+                It should be a json encoded string:
+                payload = json.dumps(["Hello world one", "hello world two"])
+                or directly the string to translate.
 
+        Returns:
+            Any: _description_
+        """
+        url = urljoin(self._spanglish_url, endpoint)
+        with requests.Session() as session:
+            response = session.request("GET", url, params={"text": payload})
+            try:
+                return response.json()
+            except Exception as exc:
+                raise ValueError("Unexpected error on the response") from exc
