b95757f3c9f7ce352c332f49639b983d3e7af5f6
agustin <agustin.piqueres@gmail.com>
2023-07-12 22:44:32 +0200

    test: draft tests for pieces

--- tests/test_markdown.py
+++ tests/test_markdown.py
@@ -4,12 +4,16 @@ import pytest
 from pathlib import Path
 from translate_md import markdown as md
 import tempfile
+import textwrap
+
 
 filename = (
     Path(__file__).parent
     / "data"
     / "a-NER-model-for-command-line-help-messages-part1.en.md"
 )
+
+filename_rich = Path(__file__).parent / "data" / "README-rich.md"
 
 mdproc = md.MarkdownProcessor(md.read_file(filename))
 
@@ -25,7 +29,6 @@ def mdprocessor():
 
 
 class TestMarkdownProcessor:
-
     def test_tokens(self):
         toks = mdproc.tokens
         assert isinstance(toks, list)
@@ -45,8 +48,12 @@ class TestMarkdownProcessor:
         new_pieces = [""] * len(pieces)
         new_pieces[0] = "texto traducido"
         mdprocessor.update(new_pieces)
-        assert mdprocessor.tokens[mdprocessor._positions[0]].content == "texto traducido"
-        assert all([mdprocessor._tokens[p].content == "" for p in mdprocessor._positions[1:]])
+        assert (
+            mdprocessor.tokens[mdprocessor._positions[0]].content == "texto traducido"
+        )
+        assert all(
+            [mdprocessor._tokens[p].content == "" for p in mdprocessor._positions[1:]]
+        )
 
     def test_render(self, mdprocessor):
         # Update with silly content
@@ -55,7 +62,7 @@ class TestMarkdownProcessor:
         # new_pieces = [""] * len(pieces)
         new_pieces = ["hola"] * len(pieces)
         mdprocessor.update(new_pieces)
- 
+
         out = mdprocessor.render()
         assert len(out) == 1615
         assert "hola" in out
@@ -70,7 +77,7 @@ class TestMarkdownProcessor:
             mdprocessor.write_to(Path(tmp) / "testfile.md")
             assert (Path(tmp) / "testfile.md").is_file()
             assert len(md.read_file(Path(tmp) / "testfile.md")) == 1615
- 
+
 
 def test_is_front_matter():
     text = """---\ntitle: "A NER Model for Command Line Help Messages (Part 1: The command line program)"\ndate: 2023-02-21T18:55:27+01:00\ndraft: false\ncategories: ["NLP"]\ntags: ["NLP", "NER", "spaCy", "Python", "rich"]\n---"""
@@ -79,18 +86,174 @@ def test_is_front_matter():
 
 
 def test_is_figure():
-    text = '![helpner](/images/helpner-arch-part1.png)'
+    text = "![helpner](/images/helpner-arch-part1.png)"
     assert md.is_figure(text) is True
     assert md.is_figure(text.replace("!", "-")) is False
 
 
 def test_is_code():
-    text = '```console\n$ pip install helpner\n```'
+    text = "```console\n$ pip install helpner\n```"
     assert md.is_code(text) is True
     assert md.is_code(text.replace("`", "-")) is False
 
 
 def test_is_comment():
-    text = '<!-- ### Related posts\nadd here -->'
+    text = "<!-- ### Related posts\nadd here -->"
     assert md.is_comment(text) is True
     assert md.is_comment(text.replace("<!--", "-")) is False
+
+
+@pytest.mark.parametrize(
+    "text, length_links, has_placeholder",
+    [
+        (
+            "[English readme](https://github.com/textualize/rich/blob/master/README.md)",
+            1,
+            True,
+        ),
+        (
+            "![Logo](https://github.com/textualize/rich/raw/master/imgs/logo.svg)",
+            1,
+            True,
+        ),
+        (
+            "This is a badge [![Supported Python Versions](https://img.shields.io/pypi/pyversions/rich/13.2.0)](https://pypi.org/project/rich/).",
+            1,
+            True,
+        ),
+        (
+            "[![Supported Python Versions](https://img.shields.io/pypi/pyversions/rich/13.2.0)](https://pypi.org/project/rich/) [![PyPI version](https://badge.fury.io/py/rich.svg)](https://badge.fury.io/py/rich)",
+            2,
+            True,
+        ),
+        (
+            "Rich is a Python library for _rich_ text and beautiful formatting in the terminal.",
+            0,
+            False,
+        ),
+        (
+            "For a video introduction to Rich see [calmcode.io](https://calmcode.io/rich/introduction.html) by [@fishnets88](https://twitter.com/fishnets88).",
+            2,
+            True,
+        ),
+    ],
+)
+def test_grab_mdlinks(text, length_links, has_placeholder):
+    text, replaced = md.replace_links(text)
+    assert len(replaced) == length_links
+    assert (md.PLACEHOLDER_MDLINK in text) is has_placeholder
+
+
+# parser_name = "commonmark"
+parser_name = "zero"
+
+mdproc_rich = md.MarkdownProcessor(md.read_file(filename_rich), parser_name=parser_name)
+
+
+@pytest.fixture
+def mdprocessor_rich():
+    return md.MarkdownProcessor(md.read_file(filename_rich), parser_name=parser_name)
+
+
+class TestMarkdownProcessorNew:
+    def test_tokens(self):
+        toks = mdproc_rich.tokens
+        assert isinstance(toks, list)
+        assert isinstance(toks[0], md.Token)
+
+    def test_repr(self):
+        assert repr(mdproc_rich) == "MarkdownProcessor(332)"
+
+    def test_get_pieces(self):
+        pieces = mdproc_rich.get_pieces()
+        assert len(pieces) == 74
+        print(pieces[:3])
+        assert all([isinstance(p, list) and len(p) > 0 for p in pieces])
+        print("PIECES", pieces[4])
+        assert len(pieces[4]) == 2
+
+    def test_replace_links(self, mdprocessor_rich):
+        assert len(mdprocessor_rich._replaced) == 0
+        pieces = mdprocessor_rich.get_pieces()
+        replaced = mdprocessor_rich._replaced
+        assert len(replaced) == 74
+        assert isinstance(replaced[0], list)
+        assert isinstance(replaced[0][0], list)
+        assert len(replaced[0]) == 1
+        assert len(replaced[0][0]) == 2
+
+        assert len(replaced[1]) == 4
+        assert len(replaced[1][0]) == 1
+
+        assert len(replaced[2]) == 18
+        assert len(replaced[2][0]) == 1
+
+        assert len(replaced[4]) == 1
+
+        assert len(replaced[5]) == 1
+
+    # def test_update(self, mdprocessor_rich):
+    #     pieces = mdprocessor_rich.get_pieces()
+    #     with pytest.raises(ValueError):
+    #         mdprocessor_rich.update(["wrong length"])
+    #     new_pieces = [""] * len(pieces)
+    #     new_pieces[0] = "texto traducido"
+    #     mdprocessor_rich.update(new_pieces)
+    #     assert (
+    #         mdprocessor_rich.tokens[mdprocessor_rich._positions[0]].content == "texto traducido"
+    #     )
+    #     assert all(
+    #         [mdprocessor_rich._tokens[p].content == "" for p in mdprocessor_rich._positions[1:]]
+    #     )
+
+    # def test_render(self, mdprocessor_rich):
+    #     # Update with silly content
+    #     pieces = mdprocessor_rich.get_pieces()
+    #     assert len(mdprocessor_rich._content) == 6250
+    #     # new_pieces = [""] * len(pieces)
+    #     new_pieces = ["hola"] * len(pieces)
+    #     mdprocessor_rich.update(new_pieces)
+
+    #     out = mdprocessor_rich.render()
+    #     assert len(out) == 1615
+    #     assert "hola" in out
+
+    # def test_write_to(self, mdprocessor_rich):
+    #     # Update with silly content
+    #     pieces = mdprocessor_rich.get_pieces()
+    #     new_pieces = ["hola"] * len(pieces)
+    #     mdprocessor_rich.update(new_pieces)
+
+    #     with tempfile.TemporaryDirectory() as tmp:
+    #         mdprocessor_rich.write_to(Path(tmp) / "testfile.md")
+    #         assert (Path(tmp) / "testfile.md").is_file()
+    #         assert len(md.read_file(Path(tmp) / "testfile.md")) == 1615
+
+
+class TestPiece:
+    @pytest.fixture(scope="class")
+    def badge(self):
+        return textwrap.dedent(
+            """
+        [![Supported Python Versions](https://img.shields.io/pypi/pyversions/rich/13.2.0)](https://pypi.org/project/rich/) [![PyPI version](https://badge.fury.io/py/rich.svg)](https://badge.fury.io/py/rich)        """
+        )
+
+    @pytest.fixture(scope="class")
+    def paragraph_with_links(self):
+        return textwrap.dedent(
+            """
+        For a video introduction to Rich see [calmcode.io](https://calmcode.io/rich/introduction.html) by [@fishnets88](https://twitter.com/fishnets88)."""
+        )
+
+    @pytest.fixture(scope="class")
+    def section(self):
+        return "## Compatibility"
+
+    @pytest.fixture(scope="class")
+    def section_heading(self):
+        return "## [Documentation](https://readthedocs.io)"
+
+    def test_piece_badge(self, badge):
+        piece = md.Piece(badge, 0)
+        assert piece.section_header is True
+        assert badge == 1
